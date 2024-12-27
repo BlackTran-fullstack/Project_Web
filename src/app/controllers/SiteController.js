1027f27ce417bffc6e4b48804b1fb461229db2b7
pmgbao2004@gmail.com <pmgbao2004@gmail.com>
2024-12-27 18:06:08 +0700

    checkout and payment feature

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -709,6 +709,52 @@ class SiteController {
             });
         }
     }
+
+    // [GET] /list-orders
+    async listOrders(req, res) {
+        try {
+            const userId = req.user.id;
+            const orders = await Orders.find({ userId })
+            .sort({ createdAt: -1 })
+            res.render("listOrders", {
+                orders: mutipleMongooseToObject(orders),
+            });
+        } catch (error) {
+            console.error("Error in listOrders:", error);
+            res.status(500).json({
+                success: false,
+                errors: ["Server error. Please try again later."],
+            });
+        }
+    }
+
+    // [GET] /list-orders/:orderId
+    async orderDetails(req, res) {
+        try {
+            const orderId = req.params.orderId;
+            const orderDetails = await OrderDetails.find({ orderId });
+            let products = [];
+            for (const item of orderDetails) {
+                const product = await Products.findOne({ _id: item.productId });
+                console.log("product:", product);
+                products.push({
+                    name: product.name,
+                    price: product.price,
+                    quantity: item.quantity,
+                });
+            }
+            res.render("orderDetail", 
+            {
+                products: products,
+            });
+        } catch (error) {
+            console.error("Error in orderDetails:", error);
+            res.status(500).json({
+                success: false,
+                errors: ["Server error. Please try again later."],
+            });
+        }
+    }
 }
 
 module.exports = new SiteController();
